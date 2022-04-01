4fdb60300b0292e82a029c16821a6c9cc8ac97ba
MouchenHung <Mouchen.Hung@quantatw.com>
2022-04-01 17:30:29 +0800

    commit-20220401-Add do update code

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifdef __UINT8_TYPE__
 typedef __UINT8_TYPE__ uint8_t;
+#endif
+
+#ifdef __UINT16_TYPE__
+typedef __UINT16_TYPE__ uint16_t;
 #endif
 
 #ifdef __UINT32_TYPE__
@@ -11,15 +16,29 @@ typedef __UINT32_TYPE__ uint32_t;
 
 #define PROJ_NAME "FW UPDATE TOOL"
 #define PROJ_DESCRIPTION "Firmware update tool from host, including [BIC] [BIOS] [CPLD]."
-#define PROJ_VERSION "v1.0"
-#define PROJ_DATE "2022.03.30"
+#define PROJ_VERSION "v1.0.1"
+#define PROJ_DATE "2022.04.01"
 #define PROJ_AUTH "Quanta"
 
 #define MAX_IMG_LENGTH 0x80000
-#define MAX_IPMI_DATA_SIZE 244
+#define MAX_IPMB_SIZE 244
+#define MAX_IPMB_DATA_SIZE 224
 #define SECTOR_SZ_64K 0x10000
 
-#define DEBUG_LOG 0
+/* ipmi-raw firmware update */
+#define FW_UPDATE_NETFN 0x38
+#define FW_UPDATE_CMD 0x09
+#define FW_UPDATE_LUN 0x00
+#define PREFIX_IPMI_RAW "./ipmi-raw"
+
+/* IANA for QUANTA oem command */
+#define OEM_38 0x38
+#define OEM_36 0x36
+#define IANA_1 0x9C
+#define IANA_2 0x9C
+#define IANA_3 0x00
+
+int DEBUG_LOG = 0;
 
 typedef enum fw_type {
     FW_T_BIC,
@@ -30,10 +49,18 @@ typedef enum fw_type {
 
 char *IMG_TYPE_LST[3] = {"BIC", "BIOS", "CPLD"};
 
+typedef struct fw_update_data {
+    uint8_t target;
+    uint8_t offset[4];
+    uint8_t length[2];
+    uint8_t data[MAX_IPMB_DATA_SIZE];
+} fw_update_data_t;
+
 typedef struct ipmi_cmd {
     uint8_t netfn; /* note whether include LUN */
     uint8_t cmd;
-    uint8_t data[MAX_IPMI_DATA_SIZE];
+    uint8_t data[MAX_IPMB_SIZE];
+    uint32_t data_len;
 }ipmi_cmd_t;
 
 uint32_t read_binary(char *bin_path, uint8_t *buff, uint32_t buff_len) {
@@ -41,7 +68,7 @@ uint32_t read_binary(char *bin_path, uint8_t *buff, uint32_t buff_len) {
         return 0;
 
     FILE *ptr;
-    size_t bin_size; /*filesize*/
+    uint32_t bin_size; /*filesize*/
 
     ptr = fopen(bin_path,"rb");  // r for read, b for binary
     if (!ptr) {
@@ -61,7 +88,7 @@ uint32_t read_binary(char *bin_path, uint8_t *buff, uint32_t buff_len) {
 
     fread(buff, buff_len, 1, ptr); // read 10 bytes to our buffer
 
-    if (DEBUG_LOG) {
+    if (DEBUG_LOG >= 3) {
         for (int i=0; i<bin_size; i++)
             printf("[0x%x] ", buff[i]);
         printf("\n");
@@ -71,22 +98,163 @@ uint32_t read_binary(char *bin_path, uint8_t *buff, uint32_t buff_len) {
     return bin_size;
 }
 
-int send_recv_command() {
-    /* TODO: Send command here */
+void dummy_ipmi_raw() {
+    return 0;
+}
 
-    /* TODO: Wait Receive command here */
+int send_recv_command(ipmi_cmd_t *msg) {
+    if (DEBUG_LOG >= 2) {
+        /* common print */
+        printf("     * ipmi command     : 0x%x/0x%x\n", msg->netfn, msg->cmd);
+        printf("     * ipmi data length : %d\n", msg->data_len);
+        printf("     * ipmi data        : ");
+        int max_data_print = 10;
+
+        if (msg->data_len <= max_data_print)
+            max_data_print = msg->data_len;
+
+        for (int i=0; i<max_data_print; i++)
+            printf("0x%x ", msg->data[i]);
+        printf("...\n");
+    }
+
+    int oem_flag = 0;
+
+    /* IPMI-RAW string - [LUN] [NETFN] [COMMAND] */
+    char ipmi_common[3][5];
+    sprintf(ipmi_common[0], "0x%x", msg->netfn & 0x03);
+    sprintf(ipmi_common[1], "0x%x", msg->cmd);
+    sprintf(ipmi_common[2], "0x%x", msg->netfn >> 2);
+
+    if ( (msg->netfn >> 2) == OEM_36 || (msg->netfn >> 2) == OEM_38) {
+        msg->data_len += 3;
+        if (msg->data_len > MAX_IPMB_SIZE)
+            return 1;
+        oem_flag = 1;
+    }
+
+    /* IPMI-RAW string - [DATA] */
+    char ipmi_data[msg->data_len][5];
+    int init_idx = 0;
+    if (oem_flag) {
+        sprintf(ipmi_data[0], "0x%x", IANA_1);
+        sprintf(ipmi_data[1], "0x%x", IANA_2);
+        sprintf(ipmi_data[2], "0x%x", IANA_3);
+        init_idx += 3;
+    }
+    for (int i=init_idx; i<msg->data_len; i++) {
+        sprintf(ipmi_data[i], "0x%x", msg->data[i-init_idx]);
+    }
+
+    /* TODO: Make command string, then send out command */
+/*
+    char *img_tx_buf[5*(sizeof(ipmi_common) + sizeof(ipmi_data))];
+    // append prefix
+    asprintf(&img_tx_buf[0], PREFIX_IPMI_RAW);
+    // append common part
+    for (int i=1; i<(sizeof(ipmi_common)/5); i++) {
+        asprintf(&img_tx_buf[i], ipmi_common[i-1]);
+    }
+    // append data part
+    for (int i=1+(sizeof(ipmi_common)/5); i<(sizeof(ipmi_data)/5); i++) {
+        asprintf(&img_tx_buf[i], ipmi_data[i-1-(sizeof(ipmi_common)/5)]);
+    }
+
+    // send ipmi-raw command out
+    if (ipmi_raw_main( (sizeof(ipmi_common)+sizeof(ipmi_data))/5 + 1, img_tx_buf)) {
+        return 1;
+    }
+*/
 
     return 0;
 }
 
 int do_bic_update(uint8_t *buff, uint32_t buff_len) {
-    /* TODO: Write update code here */
-
-    if ( send_recv_command() )
+    if (!buff)
         return 1;
 
-    if ( send_recv_command() )
-        return 1;
+    uint32_t cur_msg_offset = 0;
+    uint8_t *cur_buff = buff;
+    uint8_t last_cmd_flag = 0;
+    uint32_t section_offset = 0;
+    uint16_t section_idx = 0;
+    uint8_t percent;
+
+    uint16_t msg_len;
+    if (buff_len > MAX_IPMB_DATA_SIZE) {
+        msg_len = MAX_IPMB_DATA_SIZE;
+    }else {
+        msg_len = buff_len;
+        last_cmd_flag = 1;
+    }
+
+    while(cur_msg_offset < buff_len) {
+        if (section_offset == SECTOR_SZ_64K) {
+            section_offset = 0;
+            section_idx++;
+        }
+
+        /* If current size over 64K */
+        if ( (section_offset + MAX_IPMB_DATA_SIZE) / SECTOR_SZ_64K )
+            msg_len = (SECTOR_SZ_64K - section_offset);
+        else
+            msg_len = MAX_IPMB_DATA_SIZE;
+
+        /* If next msg offset over given img length */
+        if ( (cur_msg_offset + msg_len) >= buff_len) {
+            msg_len = (buff_len - cur_msg_offset);
+            last_cmd_flag = 1;
+        }
+
+        /* SEND COMMAND HERE */
+        fw_update_data_t cmd_data;
+        if (last_cmd_flag)
+            cmd_data.target = 0x82;
+        else
+            cmd_data.target = 0x02;
+
+        cmd_data.offset[0] = (cur_msg_offset & 0xFF);
+        cmd_data.offset[1] = (cur_msg_offset >> 8) & 0xFF;
+        cmd_data.offset[2] = (cur_msg_offset >> 16) & 0xFF;
+        cmd_data.offset[3] = (cur_msg_offset >> 24) & 0xFF;
+
+        cmd_data.length[0] = msg_len & 0xFF;
+        cmd_data.length[1] = (msg_len >> 8) & 0xFF;
+
+        if ( percent != (cur_msg_offset+msg_len)*100/buff_len ) {
+            percent = (cur_msg_offset+msg_len)*100/buff_len;
+            if (!(percent % 5))
+                printf("         update status %d%\n", percent);
+        }
+        //memcpy(cmd_data.length, &msg_len, sizeof(cmd_data.length));
+        //memcpy(cmd_data.data, cur_buff, msg_len);
+
+        ipmi_cmd_t msg_out;
+        msg_out.netfn = FW_UPDATE_NETFN << 2;
+        msg_out.cmd = FW_UPDATE_CMD;
+        msg_out.data_len = msg_len+7;
+        memcpy(msg_out.data, &cmd_data, msg_len+7); /* todo */
+
+        if (DEBUG_LOG >= 1) {
+            printf("<debug> section_idx[%d] section_offset[0x%x/0x%x] image_offset[0x%x]\n",
+                   section_idx, section_offset, SECTOR_SZ_64K, cur_msg_offset);
+            /* custom print for each command */
+            printf("        target[0x%x] offset[0x%x] size[%d]\n",
+                    msg_out.data[0],
+                    msg_out.data[1]|(msg_out.data[2] << 8)|(msg_out.data[3] << 16)|(msg_out.data[4] << 24),
+                    msg_out.data[5]|(msg_out.data[6] << 8));
+        }
+
+        if ( send_recv_command(&msg_out) )
+            return 1;
+
+        cur_msg_offset += msg_len;
+        cur_buff += msg_len;
+        section_offset += msg_len;
+
+        if (cur_msg_offset == (SECTOR_SZ_64K + 1000))
+            break;
+    }
 
     return 0;
 }
@@ -104,11 +272,16 @@ int fw_update(fw_type_t flag, uint8_t *buff, uint32_t buff_len) {
             return 1;
         break;
     case FW_T_BIOS:
+        printf("<warn> BIOS update hasn't support yet!\n");
+        return 1;
         break;
     case FW_T_CPLD:
+        printf("<warn> CPLD update hasn't support yet!\n");
+        return 1;
         break;
     default:
         printf("<error> fw_update: No such flag!\n");
+        return 1;
         break;
     }
 
@@ -116,27 +289,38 @@ int fw_update(fw_type_t flag, uint8_t *buff, uint32_t buff_len) {
 }
 
 void HELP() {
-    printf("Try: ./host_fw_update <fw_type> <img_path>\n");
-    printf("     <fw_type>  Firmware type [0]BIC [1]BIOS [2]CPLD\n");
-    printf("     <img_path> Image path\n\n");
+    printf("Try: ./host_fw_update <fw_type> <img_path> <log_level>\n");
+    printf("     <fw_type>   Firmware type [0]BIC [1]BIOS [2]CPLD\n");
+    printf("     <img_path>  Image path\n");
+    printf("     <log_level> (optional) Log level [-v]L1 [-vv]L2 [-vvv]L3\n\n");
 }
 
 void HEADER_PRINT() {
-    printf("================================================================\n");
+    printf("===============================================================================\n");
     printf("* Name         : %s\n", PROJ_NAME);
     printf("* Description  : %s\n", PROJ_DESCRIPTION);
     printf("* Ver/Date     : %s/%s\n", PROJ_VERSION, PROJ_DATE);
     printf("* Author       : %s\n", PROJ_AUTH);
-    printf("================================================================\n");
+    printf("* Note         : %s\n", "none");
+    printf("===============================================================================\n");
 }
 
 int main(int argc, const char** argv)
 {
     HEADER_PRINT();
 
-    if (argc!=3) {
+    if (argc!=3 && argc!=4) {
         HELP();
         return 0;
+    }
+
+    if (argc == 4) {
+        if (strstr(argv[3], "-v"))
+            DEBUG_LOG = 1;
+        if (strstr(argv[3], "-vv"))
+            DEBUG_LOG = 2;
+        if (strstr(argv[3], "-vvv"))
+            DEBUG_LOG = 3;
     }
 
     int img_idx = atoi(argv[1]);
@@ -148,7 +332,12 @@ int main(int argc, const char** argv)
         return 0;
     }
 
-    printf("<system> Start [%s] update task with image [%s]\n", IMG_TYPE_LST[img_idx], img_path);
+    if (!DEBUG_LOG)
+        printf("<system> Detail ignore...\n");
+    else
+        printf("<system> Detail leven %d...\n", DEBUG_LOG);
+
+    printf("\n<system> Start [%s] update task with image [%s]\n", IMG_TYPE_LST[img_idx], img_path);
 
     uint8_t *img_buff = malloc(sizeof(uint8_t) * MAX_IMG_LENGTH);
     if (!img_buff) {
@@ -157,23 +346,26 @@ int main(int argc, const char** argv)
     }
 
     /* STEP1 */
-    printf("<system> STEP1. Read image\n");
+    printf("\n<system> STEP1. Read image\n");
     uint32_t img_size = read_binary(img_path, img_buff, MAX_IMG_LENGTH);
     if (!img_size) {
-        printf("    --> failed!\n");
-        return 0;
+        printf("\n<system> Update failed!\n");
+        goto ending;
     }
-    printf("    --> success!\n");
+    printf("<system> PASS!\n");
 
     /* STEP2 */
-    printf("<system> STEP2. Upload image\n");
+    printf("\n<system> STEP2. Upload image\n");
     if ( fw_update(img_idx, img_buff, img_size) ) {
-        printf("    --> failed!\n");
-        return 0;
+        printf("\n<system> Update failed!\n");
+        goto ending;
     }
-    printf("    --> success!\n");
+    printf("<system> PASS!\n");
 
-    printf("\n\n<system> Update complete!\n");
+    printf("\n<system> Update complete!\n");
 
+ending:
+    if (img_buff)
+        free(img_buff);
     return 0;
 }
